a36c6345c53d18883201f32b4b826452d7058960
HARSH RAKHOLIYA <116006662+harshrakholiya1@users.noreply.github.com>
2023-02-25 12:48:51 -0500

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,8 @@
+# SImple Python Program To Add Two Number
+
+number1 = input("Enter The First Number :-")
+number2 = input("Enter The Second Number :-")
+
+#Using The Arithmetic + Operator
+sum = float(number1) + float(number2)
+print('The Sum Of {0} And {1} Is {2}'.format(number1,number2,sum))
